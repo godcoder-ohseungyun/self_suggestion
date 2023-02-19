4563e56baf85814ed3d70f9865dc1855ffe04a35
오승윤 <>
2023-02-19 16:20:30 +0900

    feat : flutter_local_notifications 설정

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,22 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // iOS 10 or later
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+        // Enable or disable features based on authorization.
+    }
+
+    application.registerForRemoteNotifications()
+
+    let settings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+    application.registerUserNotificationSettings(settings)
+    application.registerForRemoteNotifications()
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
+
+
