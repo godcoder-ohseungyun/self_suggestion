418c5822dcb26c43f4c4dd51feb526363e95d7b2
오승윤 <>
2023-03-06 14:43:35 +0900

    feat (Suggestions) : 사용자 입력(자기 암시 문장)을 CRD하는 기능
    싱글톤
    터미네이트 이후에도 데이터 유지( shared_preferences 플러그인 설치 )
    getEntriesList() 에서 정렬 후 반환 ( value가 true인게 우선순위이며, value가 같은 경우 key값을 기준으로 오름차순 정렬하도록  sort )

---
+++ lib/model/Suggestions.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+/**
+ * 싱글톤
+ * 터미네이트 이후에도 데이터 유지
+ */
+class Suggestions {
+  static final Suggestions _singleton = Suggestions._internal();
+  late SharedPreferences _prefs;
+
+  factory Suggestions() {
+    return _singleton;
+  }
+
+  Suggestions._internal() {
+    _initPrefs();
+  }
+
+  void _initPrefs() async {
+    _prefs = await SharedPreferences.getInstance();
+    suggestions = Map<String, bool>.from(jsonDecode(_prefs.getString('suggestions') ?? '{}'));
+  }
+
+  Map<String, bool> suggestions = {};
+
+  List<String> getKeyListOnlyValueTrue() {
+    List<String> list = List.unmodifiable(suggestions.entries
+        .where((entry) => entry.value == true)
+        .map((entry) => entry.key)
+        .toList());
+    return list;
+  }
+
+  List<MapEntry<String, bool>> getEntriesList() {
+    List<MapEntry<String, bool>> result = suggestions.entries.toList();
+    result.sort((e1, e2) {
+      if (e1.value == e2.value) {
+        return e1.key.compareTo(e2.key);
+      } else {
+        if(e1.value && !e2.value){
+          return -1;
+        }else {
+          return 1;
+        }
+      }
+    });
+    return List.unmodifiable(result);
+  }
+
+  bool add(String text) {
+    if (suggestions.containsKey(text)) {
+      return false;
+    }
+    suggestions[text] = false;
+    _savePrefs();
+    return true;
+  }
+
+  remove(String key) {
+    suggestions.remove(key);
+    _savePrefs();
+  }
+
+  check(String key) {
+    suggestions[key] = true;
+    _savePrefs();
+  }
+
+  unCheck(String key) {
+    suggestions[key] = false;
+    _savePrefs();
+  }
+
+  void _savePrefs() {
+    _prefs.setString('suggestions', jsonEncode(suggestions));
+  }
+}
