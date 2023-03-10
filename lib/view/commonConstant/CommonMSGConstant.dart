51fa6e68a6da7317d2a5f81ff0917ac8976bf3af
오승윤 <>
2023-03-10 13:58:55 +0900

    feat (CommonMSGConstant) : 클래스 공통 메세지 상수 관리 클래스 추가

---
+++ lib/view/commonConstant/CommonMSGConstant.dart
@@ -0,0 +1,6 @@
+class CommonMSGConstant{
+  static final String APP_BAR_TITLE  = "MEMO MIND";
+  static final String DELETE_MSG = "delete";
+  static final String CANCEL = "취소";
+  static final String SAVE = "저장";
+}
