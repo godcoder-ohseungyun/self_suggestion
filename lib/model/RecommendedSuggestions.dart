e99703ab065a597fc9eb2271776fa21200abf57e
오승윤 <>
2023-03-11 15:02:14 +0900

    feat (RecommendedSuggestions) : 추천 자기 암시 문구를 제공하는 기능 구현

---
+++ lib/model/RecommendedSuggestions.dart
@@ -0,0 +1,40 @@
+import 'package:self_suggestion/model/Suggestions.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+class RecommendedSuggestions {
+
+  late List<String> recommendedSuggestions;
+  Suggestions suggestions = Suggestions();
+
+  static final RecommendedSuggestions _instance = RecommendedSuggestions._internal();
+
+  factory RecommendedSuggestions() {
+    return _instance;
+  }
+
+  RecommendedSuggestions._internal(){
+    recommendedSuggestions = [];
+  }
+
+  //반드시 앱 실행 시점에 호출 해야함
+  Future<void> callRecommendedSuggestions() async {
+    final fileContent = await rootBundle.loadString('txt/recommendedSuggestions.txt');
+
+    recommendedSuggestions = fileContent.split('\n');
+  }
+
+
+  void addToSuggestions(int index){
+    suggestions.add(recommendedSuggestions[index]);
+  }
+
+  List<String> get(){
+    return List.unmodifiable(recommendedSuggestions);
+  }
+
+  void delete(int index){
+    recommendedSuggestions.removeAt(index);
+  }
+
+}
+
